bb9e401c2d573fea08c94d539b1fa977b28f5426
rohithimmadisetty <43609611+rohithimmadisetty@users.noreply.github.com>
2022-06-27 11:30:13 +0530

    Update Solution_NorthWind_Intermediate.sql

--- Intermediate/Solution_NorthWind_Intermediate.sql
+++ Intermediate/Solution_NorthWind_Intermediate.sql
@@ -2,23 +2,23 @@
             count(product_id) as total_no_of_products
             from products
             join categories
-				on products.category_id = categories.category_id
-			group by category_name
-			order by total_no_of_products desc;
-21.	 select country,
+		on products.category_id = categories.category_id
+            group by category_name
+	    order by total_no_of_products desc;
+21.  select country,
             city,
             count(customer_id) as TotalCustomer
             from customers
             group by country, city
             order by country;
-22.	 select product_id,
+22.  select product_id,
             product_name,
             units_in_stock,
             reorder_level
             from products
             where units_in_stock <  reorder_level 
             order by product_id;
-23.	 select product_id,
+23.  select product_id,
             product_name,
             units_in_stock,
             units_on_order,
@@ -28,7 +28,7 @@
             where units_in_stock + units_on_order <  reorder_level and
                   discontinued = 0
             order by product_id;
-24.	  select customer_id,
+24.   select customer_id,
              company_name,
              region,
              case
@@ -38,17 +38,17 @@
              from customers
              order by reg desc , region, company_name;
   25. select ship_country,
-			 avg(freight) as avg_fright 
+             avg(freight) as avg_fright 
              from orders
              group by ship_country
              order by avg_fright desc limit 3;
-      select ship_country,
-			 avg(freight) as avg_fright
+26.   select ship_country,
+             avg(freight) as avg_fright
              from orders
              where order_date > '1996-01-01'
              group by ship_country
              order by avg_fright desc limit 3;
-29.	 select employees.employee_id,
+29.  select employees.employee_id,
             employees.last_name,
             orders.order_id,
             product_name,
@@ -66,16 +66,16 @@
              from customers
              left join orders
                   on customers.customer_id = orders.customer_id
-			 where ISNULL(order_id);
+	     where ISNULL(order_id);
 
 31. 	select distinct(customers.customer_id) 
                from customers
                where customers.customer_id not in (select distinct(customers.customer_id) as custom
-														  from
+		                                    	  from
                                                           customers
                                                           left join orders
-                                                          on customers.customer_id = orders.customer_id
-			                                              where orders.employee_id = 4);
+                                                                on customers.customer_id = orders.customer_id
+			                                  where orders.employee_id = 4);
              
                   
              
