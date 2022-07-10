2db30d4671459a53285dac597abe74d6d57407cd
rohithimmadisetty <43609611+rohithimmadisetty@users.noreply.github.com>
2022-07-10 18:26:24 +0530

    Update Solution_NorthWind_Advanced.sql
    Added Problem as well

--- Advanced/Solution_NorthWind_Advanced.sql
+++ Advanced/Solution_NorthWind_Advanced.sql
@@ -1,4 +1,7 @@
-32. with cte as( select  customers.customer_id,
+32. High-value customers 
+    We want to send all of our high-value customers a special VIP gift. We're defining high-value customers as those who've made at least 1 order with a total value (not including the discount) equal to $10,000 or more. We only want to consider orders made in the year 2016.
+
+    with cte as( select  customers.customer_id,
                		 SUM(order_details.unit_price * order_details.quantity  * (1 - order_details.discount)) As Total
 		 	 from customers
          		 join orders
@@ -14,8 +17,11 @@
 		where total > 10000
                 order by total desc;
 		 
-33.with cte as( select customers.customer_id,
-                       SUM(order_details.unit_price * order_details.quantity  * (1 - order_details.discount)) As Total
+33.High-value customers - total orders 
+   The manager has changed his mind. Instead of requiring that customers have at least one individual orders totaling $10,000 or more, he wants to define high-value customers as those who have orders totaling $15,000 or more in 2016. How would you change the answer to the problem above?
+
+    with cte as( select customers.customer_id,
+                        SUM(order_details.unit_price * order_details.quantity  * (1 - order_details.discount)) As Total
 		 	from customers
          		join orders
               		  on customers.customer_id = orders.customer_id
@@ -30,7 +36,9 @@
 		where total > 15000
                 order by total desc
 		 ;
-34.   with cte as(select customers.customer_id,
+34.High-value customers - with discount 
+   Change the above query to use the discount when calculating high-value customers. Order by the total amount which includes the discount.
+      with cte as(select customers.customer_id,
            		 order_details.order_id as orderid,
         		 SUM(order_details.unit_price * order_details.quantity  * (1 - order_details.discount)) As Total
 		 	 from customers
@@ -47,12 +55,18 @@
 		where total > 10000
                 order by total desc;
 		 
- 36.select order_id,
+ 36. Orders with many line items 
+     The Northwind mobile app developers are testing an app that customers will use to show orders. In order to make sure that even the largest orders will show up correctly on the app, they'd like some samples of orders that have lots of individual line items. Show the 10 orders with the most line items, in order of total line items.
+
+   select order_id,
           count(product_id) as Total_Order_details
           from order_details
           group by order_id
           order by total_order_details desc;
- 38.with cte as ( Select  Order_ID,
+ 38.Orders - accidental double-entry 
+    Janet Leverling, one of the salespeople, has come to you with a request. She thinks that she accidentally double-entered a line item on an order, with a different ProductID, but the same quantity. She remembers that the quantity was 60 or more. Show all the OrderIDs with line items that match this, in order of OrderID.
+
+    with cte as ( Select  Order_ID,
           		  Product_ID,
       			  Quantity,
          		  count(quantity) as coun
@@ -65,7 +79,9 @@
                  from
                  cte 
                  where cte.coun > 1;
-39. with cte as ( Select  Order_ID,
+39.Orders - accidental double-entry details
+   Based on the previous question, we now want to show details of the order, for orders that match the above criteria.
+    with cte as ( Select  Order_ID,
           		  Product_ID,
       			  Quantity,
          		  count(quantity) as coun
@@ -77,7 +93,10 @@
           select * 
                  from order_details
                  where order_details.order_id in (select order_id from cte where cte.coun > 1);
-40. with cte as ( Select  Order_ID,
+40.Orders - accidental double-entry details, derived table
+   Here's another way of getting the same results as in the previous problem, using a derived table instead of a CTE. However, there's a bug in this SQL. It returns 20 rows instead of 16. Correct the SQL.
+
+    with cte as ( Select  Order_ID,
           		  Product_ID,
           		  Quantity,
           	          count(quantity) as coun
@@ -90,7 +109,10 @@
                  from
                  cte 
                  where cte.coun > 1;
-41.  select order_id,
+41.Late orders 
+   Some customers are complaining about their orders arriving late. Which orders are late?
+
+     select order_id,
             order_date,
             required_date,
             shipped_date
